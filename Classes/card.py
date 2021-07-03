058af85266a0461b5e21c1f2e27848dba6ea7bd3
jkutkut <jorgeregonzalez@gmail.com>
2021-07-03 23:15:14 +0200

    methods commented

--- Classes/card.py
+++ Classes/card.py
@@ -1,23 +1,31 @@
 class Card:
     SUIT = {}
     RANK = {}
+
     def __init__(self, rank, suit) -> None:
-        if self.__class__ == Card:
+        if self.__class__ == Card: # If constructor called from this class
             raise Exception("The class Card can not be initialized, it is a prototype for the rest of the classes.")
+        
+        # Check arguments
         if not self.isValidRank(rank):
             raise Exception(f"Not valid rank. It must be {self.RANK['MIN']} <= rank <= {self.RANK['MIN']}")
+        
         if not self.isValidSuit(suit):
             raise Exception(f"Not valid suit. It must be {', '.join([i for i in self.SUIT.keys()])}.\n Therefore, 0 <= suit <= {len(self.SUIT)}")
         
-
+        # Store them (valid)
         self.rank = rank
         self.suit = suit
-        pass
 
-    def isValidRank(self, rank):
+    def isValidRank(self, rank) -> bool:
+        '''If the rank given is a valid integer with a correct value.'''
         return isinstance(rank, int) and rank >= self.RANK["MIN"] and rank <= self.RANK["MAX"]
-    def isValidSuit(self, suit):
+    
+    def isValidSuit(self, suit) -> bool:
+        '''If the suit given is a valid one. It must be the integer equivalent.'''
         return isinstance(suit, int) and suit >= 0 and suit <= len(self.SUIT)
+
+
 
 class PokerCard(Card):
     SUIT = {
