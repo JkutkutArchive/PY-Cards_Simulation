8147ec25fff08d6b6f34c9c77b7443c3570e344a
jkutkut <jorgeregonzalez@gmail.com>
2021-07-03 23:41:53 +0200

    rank, suit, rankName, suitName and str methods added

--- Classes/card.py
+++ Classes/card.py
@@ -1,5 +1,6 @@
 class Card:
     SUIT = {}
+    SUITNAME = []
     RANK = {}
 
     def __init__(self, rank, suit) -> None:
@@ -14,9 +15,27 @@ class Card:
             raise Exception(f"Not valid suit. It must be {', '.join([i for i in self.SUIT.keys()])}.\n Therefore, 0 <= suit <= {len(self.SUIT)}")
         
         # Store them (valid)
-        self.rank = rank
-        self.suit = suit
+        self._rank = rank
+        self._suit = suit
 
+    # ########## GETTERS AND SETTERS ##########
+
+    def getRank(self) -> int:
+        return self._rank
+
+    def getSuit(self) -> int:
+        return self._suit
+
+    def getRankName(self) -> str:
+        return self.getRank()
+    
+    def getSuitName(self) -> str:
+        return self.SUITNAME[self.getSuit()]
+
+    def __str__(self) -> str:
+        return f"{self.getRankName()} of {self.getSuitName()}"
+
+    # ########## CHECKERS ##########
     def isValidRank(self, rank) -> bool:
         '''If the rank given is a valid integer with a correct value.'''
         return isinstance(rank, int) and rank >= self.RANK["MIN"] and rank <= self.RANK["MAX"]
@@ -24,6 +43,9 @@ class Card:
     def isValidSuit(self, suit) -> bool:
         '''If the suit given is a valid one. It must be the integer equivalent.'''
         return isinstance(suit, int) and suit >= 0 and suit <= len(self.SUIT)
+
+    
+    
 
 
 
@@ -34,10 +56,28 @@ class PokerCard(Card):
         "HEARTS": 2,
         "SPADES": 3
     }
+    SUITNAME = ["CLUBS", "DIAMONDS", "HEARTS", "SPADES"]
     RANK = {
         "MIN": 0, # 0 = Joker
         "MAX": 13
     }
+
+    # ########## GETTERS AND SETTERS ##########
+
+    def getRankName(self):
+        if self.getRank() == 0:
+            return "Poker"
+        elif self.getRank() == 1:
+            return "Ace"
+        elif self.getRank() == 11:
+            return "Jack"
+        elif self.getRank() == 12:
+            return "Queen"
+        elif self.getRank() == 13:
+            return "King"
+        return super().getRankName()
+
+
 
 class SpanishCard(Card):
     SUIT = {
@@ -46,7 +86,19 @@ class SpanishCard(Card):
         "ESPADAS": 2,
         "BASTOS": 3
     }
+    SUITNAME = ["OROS", "COPAS", "ESPADAS", "BASTOS"]
     RANK = {
         "MIN": 1,
         "MAX": 12
     }
+
+    def getRankName(self) -> str:
+        if self.getRank() == 1:
+            return "As"
+        elif self.getRank() == 10:
+            return "Sota"
+        elif self.getRank() == 11:
+            return "Caballo"
+        elif self.getRank() == 12:
+            return "Rey"
+        return super().getRankName()
