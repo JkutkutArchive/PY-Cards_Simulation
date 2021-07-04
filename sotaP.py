91f6b4a82d9bc7b91fa2bda518f84884a69ce0d5
jkutkut <jorgeregonzalez@gmail.com>
2021-07-04 02:31:12 +0200

    Constructor created and basic class shape defined

--- sotaP.py
+++ sotaP.py
@@ -1,4 +1,37 @@
+from Classes.deck import Deck
+from Classes.card import Card, SpanishCard
+from Classes.cardPlayer import CardPlayer
+from Classes.deck import Deck
+
 class SotaP:
     def __init__(self, nPlayers) -> None:
         if not isinstance(nPlayers, int) or nPlayers < 1:
             raise Exception("Number of players invalid")
+        
+        self._players = [] # Create player list
+        for i in range(nPlayers):
+            self.getPlayers().append(CardPlayer(f"Player{i+1}"))
+
+        deck = Deck(SpanishCard)
+        deck.shuffleStack()
+
+        # Give all cards to the players
+        index = 0
+        while len(deck.getStack()) > 0:
+            self.getPlayers()[index].takeCard(deck.takeCard())
+            index = (index + 1) % nPlayers
+
+        for i in range(nPlayers):
+            print(self.getPlayers()[i])
+
+
+    
+    # ########## GETTERS ##########
+    
+    def getPlayers(self) -> list:
+        '''Returns a list with the players in the game'''
+        return self._players
+
+    
+    # ########## SETTERS ##########
+    
