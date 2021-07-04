5c9fd8d0b71c52826c4fd65614df3f74702d7f00
jkutkut <jorgeregonzalez@gmail.com>
2021-07-04 10:19:55 +0200

    some bugs fixed

--- Classes/cardPlayer.py
+++ Classes/cardPlayer.py
@@ -29,9 +29,9 @@ class CardPlayer:
             raise Exception("The card is not valid")
         self.getHand().append(card)
 
-    def useCard(self, index):
+    def useCard(self, index=0):
         '''Returns the removed card at the given index.'''
-        if not isinstance(index, int) or index < 0 or index < len(self.getHand):
+        if not isinstance(index, int) or index < 0 or index >= len(self.getHand()):
             raise Exception("The index is not a valid index.")
         
         return self.getHand().pop(index)
