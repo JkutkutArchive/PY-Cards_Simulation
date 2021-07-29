87161f318b8b4538f05f38edcc2a9dc9c457edbb
jkutkut <jorgeregonzalez@gmail.com>
2021-07-29 19:00:51 +0200

    starting logic for poker game

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from poker import Poker
 from Classes.card import Card, PokerCard, SpanishCard
 from Classes.deck import Deck
+from Classes.cardPlayer import CardPlayer
 from sotaP import SotaP
 
 if __name__ == '__main__':
@@ -14,18 +16,45 @@ if __name__ == '__main__':
     # d.shuffleStack()
     # print(d)
 
-    game = SotaP(4)
+    # game = SotaP(4)
 
-    sota = game.game()
+    # sota = game.game()
 
-    for step in sota: # For each step of the game
-        # print(step)
-        pass # Do it
+    # for step in sota: # For each step of the game
+    #     # print(step)
+    #     pass # Do it
 
-    print("Game results:")
-    for i in range(len(game.gameStats["scoreBoard"])-1, -1, -1):
-        userName = game.gameStats["scoreBoard"][i].getName()
-        fastRecordIndex = game.gameStats["scoreBoard"][i].index
-        print(f"  - {userName} -> {game.gameStats['iAmThefastest'][fastRecordIndex]}")
+    # print("Game results:")
+    # for i in range(len(game.gameStats["scoreBoard"])-1, -1, -1):
+    #     userName = game.gameStats["scoreBoard"][i].getName()
+    #     fastRecordIndex = game.gameStats["scoreBoard"][i].index
+    #     print(f"  - {userName} -> {game.gameStats['iAmThefastest'][fastRecordIndex]}")
     
     # print(game.gameStats["iAmThefastest"])
+
+    print("Poker testing")
+    N = 2
+
+    players = [CardPlayer(name=f"Player{i}") for i in range(N)]
+    cards = [[] for _ in range(N)]
+
+    cards[0] = [ # High card
+        PokerCard(2, 0),
+        PokerCard(1, 1),
+        PokerCard(4, 2),
+        PokerCard(10, 2),
+        PokerCard(7, 3)
+    ]
+    cards[1] = [ # Pair
+        PokerCard(1, 0),
+        PokerCard(3, 1),
+        PokerCard(3, 2),
+        PokerCard(10, 2),
+        PokerCard(7, 3)
+    ]
+
+    for i in range(N): # for each player
+        for j in range(5): # for each card
+            players[i].takeCard(cards[i][j])
+    
+        print(Poker.analyze(players[i]))
--- poker.py
+++ poker.py
@@ -7,3 +7,12 @@ from Classes.deck import Deck
 class Poker:
     def __init__(self) -> None:
         pass
+
+    @classmethod
+    def analyze(cls, hand) -> str:
+        if not isinstance(hand, CardPlayer):
+            raise Exception("The hand given is not valid")
+        
+        hc = hand.seeCard(0)
+        print(hc)
+        print(hand)
