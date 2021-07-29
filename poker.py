0427464b132f607a4ab8f6bae7cebed7eddffd34
jkutkut <jorgeregonzalez@gmail.com>
2021-07-29 18:35:51 +0200

    Class to handle poker logic

---
+++ poker.py
@@ -0,0 +1,9 @@
+import random
+from Classes.colorOutput import *
+from Classes.card import PokerCard
+from Classes.cardPlayer import CardPlayer
+from Classes.deck import Deck
+
+class Poker:
+    def __init__(self) -> None:
+        pass
